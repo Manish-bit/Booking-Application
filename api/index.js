64a3b9f638b83c52aefa75b5e289a6b2ad3fa781
Manish-Bit <manisharyal517@gmail.com>
2023-04-13 15:27:12 +0545

    Database connection added

--- api/index.js
+++ api/index.js
@@ -1,7 +1,33 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js";
 
 const app = express();
 
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected sucessfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected");
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("mongoDD connected");
+});
+
 app.listen(8800, () => {
+  connectDB();
   console.log("Server is running");
 });
