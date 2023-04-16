4cc728d4fe4433f76a430bc32902583c0ba5a604
Manish-Bit <manisharyal517@gmail.com>
2023-04-16 15:41:01 +0545

    hotel query finished

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -27,4 +27,36 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel deleted successfully");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET HOTEL
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//GET ALL HOTEL
+
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 export default router;
