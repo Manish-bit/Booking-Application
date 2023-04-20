b1c4de274733be1e2c8a4fe6a4784a5957be31fa
Manish-Bit <manisharyal517@gmail.com>
2023-04-20 06:56:26 +0545

    verify function added

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,15 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.get("/:id", getUser);
+router.get("/", getAllUser);
 export default router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,19 @@
 import express from "express";
+import {
+  deleteUser,
+  getAllUser,
+  getUser,
+  updateUser,
+} from "../controllers/userController.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+  res.send("Hello user you are logged in");
+});
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.get("/:id", verifyUser, getUser);
+router.get("/", verifyAdmin, getAllUser);
 export default router;
