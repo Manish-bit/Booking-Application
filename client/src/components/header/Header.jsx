e6a8bca02b5a4d04f37bd58ca8a13ea04137f652
Manish-Bit <manisharyal517@gmail.com>
2023-04-30 16:42:04 +0545

    Header added

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./Header.css";
 import {
   faBed,
+  faCalendarDay,
   faCar,
+  faPerson,
   faPlane,
   faTaxi,
 } from "@fortawesome/free-solid-svg-icons";
@@ -13,7 +15,7 @@ const Header = () => {
     <div className="header">
       <div className="headerContainer">
         <div className="headerList">
-          <div className="headerListItem">
+          <div className="headerListItem active">
             <FontAwesomeIcon icon={faBed} />
             <span>Stays</span>
           </div>
@@ -32,6 +34,31 @@ const Header = () => {
           <div className="headerListItem">
             <FontAwesomeIcon icon={faTaxi} />
             <span>Stays</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">Lorem ipsum dolor sit amet</h1>
+        <p className="headerDesc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut,
+          praesentium voluptas.
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faBed} className="headerIcon" />
+            <input
+              type="text"
+              placeholder="Where are you going"
+              id=""
+              className="headerSearchInput"
+            />
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faCalendarDay} className="headerIcon" />
+            <span className="headerSearchText">date to date</span>
+          </div>
+          <div className="headerSearchItem">
+            <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+            <span className="headerSearchText">2 adults 2 children 1room</span>
           </div>
         </div>
       </div>
